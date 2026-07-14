567b7bba08122a34e4b0acef256ac95b6dcae013
Jan Raring <janraring@yahoo.de>
2026-07-14 17:29:18 +0200

    Creates data sets for the experiments
    For every language, the script deterministically picks 10 sentences of
    increasing length and stores every one of them in an individual CoNLL-U
    file.

---
+++ scripts/produce_datasets.py
@@ -0,0 +1,45 @@
+from pathlib import Path
+
+from stanza.models.common.doc import Document
+from stanza.utils.conll import CoNLL
+
+
+repo_root = Path(__file__).resolve().parents[1]
+data_root = repo_root / "data" / "external"
+test_data_destination = repo_root / "data" / "processed"
+
+for tb_dir in data_root.iterdir():
+    if not tb_dir.is_dir():
+        continue
+    if len(tb_dir.name) != 3:
+        continue
+
+    lang_name = tb_dir.name
+    print(f"\n{lang_name}:")
+
+    splits_map = {
+        "train": list(tb_dir.rglob("*train*.conllu")),
+        "dev": list(tb_dir.rglob("*dev*.conllu")),
+        "test": list(tb_dir.rglob("*test*.conllu")),
+    }
+
+    test_splits = splits_map["test"]
+    if not test_splits:
+        print(f"Found no test split for {lang_name}.")
+
+    test_doc = CoNLL.conll2doc(test_splits[0])
+    test_doc.sentences.sort(key=lambda s: len(s.text))
+
+    doc_len = len(test_doc.sentences)
+    dist = doc_len // 20
+    subset = [test_doc.sentences[(i + 1) * dist] for i in range(10)]
+
+    for sent in subset:
+        sent_path = (
+            test_data_destination / lang_name / f"{sent.sent_id}-unparsed.conllu"
+        )
+        doc = Document([])
+        doc.sentences.append(sent)
+        sent_path.parent.mkdir(parents=True, exist_ok=True)
+        CoNLL.write_doc2conll(doc, sent_path)
+        print(sent.text)
