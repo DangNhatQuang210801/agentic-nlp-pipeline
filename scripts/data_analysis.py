d8a705e55709a607a5986780548be0c1b5e29bee
Jan Raring <janraring@yahoo.de>
2026-07-19 11:16:20 +0200

    Write results to CSV

--- scripts/data_analysis.py
+++ scripts/data_analysis.py
@@ -14,20 +14,25 @@ from agentic_nlp_pipeline.experiment import (
 
 def main():
     repo_root = Path(__file__).resolve().parents[1]
+
     df_parsed_directly = compile_df(
         repo_root=repo_root,
         gold_suffix=UNPARSED,
         pred_suffix=PARSED_DIRECTLY,
         log_suffix=LOG_DIRECTLY,
+        with_tools=False,
     )
     df_parsed_agentically = compile_df(
         repo_root=repo_root,
         gold_suffix=UNPARSED,
         pred_suffix=PARSED_AGENTICALLY,
         log_suffix=LOG_AGENTICALLY,
+        with_tools=True,
     )
     df = pd.concat([df_parsed_directly, df_parsed_agentically])
-    print(df_parsed_directly)
+
+    df.to_csv(repo_root / "data" / "processed" / "parse.csv", sep="\t")
+    print(df)
 
 
 if __name__ == "__main__":
